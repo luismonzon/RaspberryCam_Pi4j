3a9e1a2f6de73ec3f0d6f1bc0b33bb94d1a90710
Luis <Luis@192.168.1.4>
2016-03-21 01:00:48 -0600

    added camera actions

---
+++ JavaApplication3/src/javaapplication3/Camara.java
@@ -0,0 +1,95 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication3;
+
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import static javaapplication3.JavaApplication3.convertidor;
+import javax.imageio.ImageIO;
+
+/**
+ *
+ * @author Luis
+ */
+
+
+
+
+public class Camara {
+    
+    public static String PICTUREFILENAME = "picam_";
+    static Runtime rt = Runtime.getRuntime();
+    static int increment;
+    public static String fileName;
+    FastRGB convertidor;
+    int[][] result;
+            
+    public Camara(){
+        
+        result=null;
+    }
+    
+    
+    public void Tomar()
+    {
+       try {   
+            System.out.println("tomando foto");
+                    fileName = "/var/www/" + PICTUREFILENAME + increment++ + ".jpg";
+                    Process p = rt.exec("raspistill -t 8000 -o " + fileName);
+                    p.waitFor();
+                    Analizar();
+          } catch (Exception e) {
+                        System.out.println("Exception:" + e.getMessage());
+                    }            
+    }
+    
+    public void Analizar()
+    {
+        
+    BufferedImage hugeImage;
+        try {
+            hugeImage = ImageIO.read(new File(fileName));
+            convertidor= new FastRGB(hugeImage);
+            System.out.println("");
+            int[][] result=null;
+            System.out.println("Testing convertTo2DWithoutUsingGetRGB:");
+
+            result = convertTo2DUsingGetRGB(hugeImage);
+      
+        } catch (IOException ex) {
+            Logger.getLogger(Camara.class.getName()).log(Level.SEVERE, null, ex);
+        }
+      /*
+      System.out.println("Testing convertTo2DUsingGetRGB:");
+      for (int i = 0; i < 10; i++) {
+         long startTime = System.nanoTime();
+         int[][] result = convertTo2DUsingGetRGB(hugeImage);
+         long endTime = System.nanoTime();
+         System.out.println(String.format("%-2d: %s", (i + 1), toString(endTime - startTime)));
+      }*/
+      
+    
+    }
+    
+   private  int[][] convertTo2DUsingGetRGB(BufferedImage image) {
+      int width = image.getWidth();
+      int height = image.getHeight();
+      int[][] result = new int[height][width];
+
+      for (int row = 0; row < height; row++) {
+         for (int col = 0; col < width; col++) {
+             
+            result[row][col] = convertidor.getRGB(col, row);
+           
+         }
+      }
+      return result;
+   }
+
+}
--- JavaApplication3/src/javaapplication3/JavaApplication3.java
+++ JavaApplication3/src/javaapplication3/JavaApplication3.java
@@ -50,49 +50,9 @@ static FastRGB convertidor;
   */
     public static void main(String[] args) throws IOException {
 
-      BufferedImage hugeImage = ImageIO.read(new File("C:\\Users\\lmonzon\\Documents\\cam.jpg"));
-      /*
-      System.out.println("Testing convertTo2DUsingGetRGB:");
-      for (int i = 0; i < 10; i++) {
-         long startTime = System.nanoTime();
-         int[][] result = convertTo2DUsingGetRGB(hugeImage);
-         long endTime = System.nanoTime();
-         System.out.println(String.format("%-2d: %s", (i + 1), toString(endTime - startTime)));
-      }*/
-      convertidor= new FastRGB(hugeImage);
-      System.out.println("");
-      int[][] result=null;
-      System.out.println("Testing convertTo2DWithoutUsingGetRGB:");
-     
-         result = convertTo2DUsingGetRGB(hugeImage);
-      
-   
-      
-        for (int i = 0; i < result.length; i++) {
-            for (int j = 0; j < result[0].length; j++) {
-                Color nuevo = new Color(result[i][j]);
-                
-                System.out.println(nuevo.getBlue()+" "+ nuevo.getGreen()+ " "+nuevo.getRed());
-            }
-        }
       
    }
     
-   private static int[][] convertTo2DUsingGetRGB(BufferedImage image) {
-      int width = image.getWidth();
-      int height = image.getHeight();
-      int[][] result = new int[height][width];
-
-      for (int row = 0; row < height; row++) {
-         for (int col = 0; col < width; col++) {
-             
-            result[row][col] = convertidor.getRGB(col, row);
-           
-         }
-      }
-      return result;
-   }
-
    
 
 }
